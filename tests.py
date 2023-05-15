4edb8ae67cf1f957e56e39265ed24a742e36dec3
Walter <walterlaicollege@gmail.com>
2023-05-14 22:04:21 -0400

    added the projections onto the singular vector

--- tests.py
+++ tests.py
@@ -8,17 +8,22 @@ y = 2*x + 1 + rng.normal(size=(8,1)) * 5
 
 A = np.hstack([x,y])
 
-plt.scatter(x,y)
-plt.show()
-
 cov_matrix = covariance(A)
 evalues, evectors = np.linalg.eig(cov_matrix)
 evectors = evectors/np.sum(evectors**2,axis=0)**0.5 #normalize
-new_coordinates = A @ evectors
+eval_order = np.argsort(evalues)[::-1]
+evalues = evalues[eval_order]
+evectors = evectors[:,eval_order]
+new_x_axis = evectors[:,0]
+new_y_axis = evectors[:,1]
+new_x_coordinates = A @ new_x_axis
+new_y_coordinates = A @ new_y_axis
+x_projections = np.outer(new_x_coordinates,new_x_axis)
 
 fig, ax =plt.subplots()
 ax.scatter(x,y,label='A')
-ax.scatter(new_coordinates[:,0], new_coordinates[:,1], label='AV')
+ax.scatter(new_x_coordinates, new_y_coordinates, label='AV')
+ax.scatter(*x_projections.T,label='New X Coordinates',marker='x')
 ax.legend()
 ax.set_aspect('equal')
 plt.show()
